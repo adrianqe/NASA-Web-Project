3938dda2257ea90cacf6fa2aeb95ae8b9be2e2b9
Adrian Quiros <157768117+adrianqe@users.noreply.github.com>
2025-04-09 15:49:15 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NASAWebPrototype.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
