e06ae610c821a356a3aa04eb3a63658bf01ac8f7
Adrian Quiros <157768117+adrianqe@users.noreply.github.com>
2025-04-16 01:07:12 -0400

    Add Experiments and Research views

---
+++ Controllers/ExperimentsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NASAWebPrototype.Controllers
+{
+    public class ExperimentsController : Controller
+    {
+        public IActionResult Experiment()
+        {
+            return View();
+        }
+        public IActionResult SimulatingLifeOnMars()
+        {
+            return View();
+        }
+        public IActionResult ManageExperiments()
+        {
+            return View();
+        }
+        public IActionResult AddExperiments()
+        {
+            return View();
+        }
+        public IActionResult SelectedExperimentSimulatingLifeOnMars()
+        {
+            return View();
+        }
+        public IActionResult SelectedExperimentMoonwalks()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ResearchProgramsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NASAWebPrototype.Controllers
+{
+    public class ResearchProgramsController : Controller
+    {
+        public IActionResult ResearchProgram()
+        {
+            return View();
+        }
+
+		public IActionResult SpaceX()
+		{
+			return View();
+		}
+        public IActionResult ManageResearchPrograms()
+        {
+            return View();
+        }
+        public IActionResult AddResearchPrograms()
+        {
+            return View();
+        }
+        public IActionResult SelectedProgramSpaceX()
+        {
+            return View();
+        }
+        public IActionResult SelectedProgramNorthrop()
+        {
+            return View();
+        }
+    }
+}
+
