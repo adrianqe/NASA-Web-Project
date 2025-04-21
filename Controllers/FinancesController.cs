894f8f71e45d5dc007d7c5d3ae8f900cde1aae62
Adrian Quiros <157768117+adrianqe@users.noreply.github.com>
2025-04-21 12:41:25 -0600

    Add Finances Module

---
+++ Controllers/FinancesController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NASAWebPrototype.Controllers
+{
+    public class FinancesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Finances()
+        {
+            return View();
+        }
+        public IActionResult ManageBudgets()
+        {
+            return View();
+        }
+        public IActionResult SpaceMissionsBudget()
+        {
+            return View();
+        }
+        public IActionResult ExperimentsBudget()
+        {
+            return View();
+        }
+        public IActionResult ResearchProgramsBudget()
+        {
+            return View();
+        }
+        public IActionResult ManageModuleBudget()
+        {
+            return View();
+        }
+        public IActionResult ManageBudget()
+        {
+            return View();
+        }
+        public IActionResult ProjectBudgets()
+        {
+            return View();
+        }
+        public IActionResult AddBudget1()
+        {
+            return View();
+        }
+        public IActionResult AddBudget2()
+        {
+            return View();
+        }
+        public IActionResult AddBudget3()
+        {
+            return View();
+        }
+        public IActionResult AddBudget4()
+        {
+            return View();
+        }
+        public IActionResult ManageBudget1()
+        {
+            return View();
+        }
+        public IActionResult ManageBudget2()
+        {
+            return View();
+        }
+        public IActionResult ManageBudget3()
+        {
+            return View();
+        }
+        public IActionResult ManageBudget4()
+        {
+            return View();
+        }
+    }
+}
